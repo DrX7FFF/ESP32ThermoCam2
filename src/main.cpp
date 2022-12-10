59448e357c282ee3dc46f2b422531b624dffc473
DrX7FFF <dubourg.v@gmail.com>
2022-12-10 11:51:54 +0100

    sauvegarde

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,18 @@
 #include "MLX90640_API.h"
 #include "MLX90640_I2C_Driver.h"
 #include "SPIFFS.h"
+#include <myfunction.h>
+#include <html.h>
+
+
 
 const byte MLX90640_address = 0x33;  // Default 7-bit unshifted address of the MLX90640
-
+paramsMLX90640 mlx90640;
+float mlx90640To[768];
 #define TA_SHIFT 8  // Default shift for MLX90640 in open air
 
-float mlx90640To[768];
-paramsMLX90640 mlx90640;
+#define I2C_SCL 14	// pb avec 12 et 13 sur ESP32 CAM
+#define I2C_SDA 2
 
 // You can use any (4 or) 5 pins
 #define sclk 18
@@ -45,272 +50,15 @@ paramsMLX90640 mlx90640;
 #define WHITE 0xFFFF
 
 // the colors we will be using
-const uint16_t camColors[] = {
-	0x480F,
-	0x400F,
-	0x400F,
-	0x400F,
-	0x4010,
-	0x3810,
-	0x3810,
-	0x3810,
-	0x3810,
-	0x3010,
-	0x3010,
-	0x3010,
-	0x2810,
-	0x2810,
-	0x2810,
-	0x2810,
-	0x2010,
-	0x2010,
-	0x2010,
-	0x1810,
-	0x1810,
-	0x1811,
-	0x1811,
-	0x1011,
-	0x1011,
-	0x1011,
-	0x0811,
-	0x0811,
-	0x0811,
-	0x0011,
-	0x0011,
-	0x0011,
-	0x0011,
-	0x0011,
-	0x0031,
-	0x0031,
-	0x0051,
-	0x0072,
-	0x0072,
-	0x0092,
-	0x00B2,
-	0x00B2,
-	0x00D2,
-	0x00F2,
-	0x00F2,
-	0x0112,
-	0x0132,
-	0x0152,
-	0x0152,
-	0x0172,
-	0x0192,
-	0x0192,
-	0x01B2,
-	0x01D2,
-	0x01F3,
-	0x01F3,
-	0x0213,
-	0x0233,
-	0x0253,
-	0x0253,
-	0x0273,
-	0x0293,
-	0x02B3,
-	0x02D3,
-	0x02D3,
-	0x02F3,
-	0x0313,
-	0x0333,
-	0x0333,
-	0x0353,
-	0x0373,
-	0x0394,
-	0x03B4,
-	0x03D4,
-	0x03D4,
-	0x03F4,
-	0x0414,
-	0x0434,
-	0x0454,
-	0x0474,
-	0x0474,
-	0x0494,
-	0x04B4,
-	0x04D4,
-	0x04F4,
-	0x0514,
-	0x0534,
-	0x0534,
-	0x0554,
-	0x0554,
-	0x0574,
-	0x0574,
-	0x0573,
-	0x0573,
-	0x0573,
-	0x0572,
-	0x0572,
-	0x0572,
-	0x0571,
-	0x0591,
-	0x0591,
-	0x0590,
-	0x0590,
-	0x058F,
-	0x058F,
-	0x058F,
-	0x058E,
-	0x05AE,
-	0x05AE,
-	0x05AD,
-	0x05AD,
-	0x05AD,
-	0x05AC,
-	0x05AC,
-	0x05AB,
-	0x05CB,
-	0x05CB,
-	0x05CA,
-	0x05CA,
-	0x05CA,
-	0x05C9,
-	0x05C9,
-	0x05C8,
-	0x05E8,
-	0x05E8,
-	0x05E7,
-	0x05E7,
-	0x05E6,
-	0x05E6,
-	0x05E6,
-	0x05E5,
-	0x05E5,
-	0x0604,
-	0x0604,
-	0x0604,
-	0x0603,
-	0x0603,
-	0x0602,
-	0x0602,
-	0x0601,
-	0x0621,
-	0x0621,
-	0x0620,
-	0x0620,
-	0x0620,
-	0x0620,
-	0x0E20,
-	0x0E20,
-	0x0E40,
-	0x1640,
-	0x1640,
-	0x1E40,
-	0x1E40,
-	0x2640,
-	0x2640,
-	0x2E40,
-	0x2E60,
-	0x3660,
-	0x3660,
-	0x3E60,
-	0x3E60,
-	0x3E60,
-	0x4660,
-	0x4660,
-	0x4E60,
-	0x4E80,
-	0x5680,
-	0x5680,
-	0x5E80,
-	0x5E80,
-	0x6680,
-	0x6680,
-	0x6E80,
-	0x6EA0,
-	0x76A0,
-	0x76A0,
-	0x7EA0,
-	0x7EA0,
-	0x86A0,
-	0x86A0,
-	0x8EA0,
-	0x8EC0,
-	0x96C0,
-	0x96C0,
-	0x9EC0,
-	0x9EC0,
-	0xA6C0,
-	0xAEC0,
-	0xAEC0,
-	0xB6E0,
-	0xB6E0,
-	0xBEE0,
-	0xBEE0,
-	0xC6E0,
-	0xC6E0,
-	0xCEE0,
-	0xCEE0,
-	0xD6E0,
-	0xD700,
-	0xDF00,
-	0xDEE0,
-	0xDEC0,
-	0xDEA0,
-	0xDE80,
-	0xDE80,
-	0xE660,
-	0xE640,
-	0xE620,
-	0xE600,
-	0xE5E0,
-	0xE5C0,
-	0xE5A0,
-	0xE580,
-	0xE560,
-	0xE540,
-	0xE520,
-	0xE500,
-	0xE4E0,
-	0xE4C0,
-	0xE4A0,
-	0xE480,
-	0xE460,
-	0xEC40,
-	0xEC20,
-	0xEC00,
-	0xEBE0,
-	0xEBC0,
-	0xEBA0,
-	0xEB80,
-	0xEB60,
-	0xEB40,
-	0xEB20,
-	0xEB00,
-	0xEAE0,
-	0xEAC0,
-	0xEAA0,
-	0xEA80,
-	0xEA60,
-	0xEA40,
-	0xF220,
-	0xF200,
-	0xF1E0,
-	0xF1C0,
-	0xF1A0,
-	0xF180,
-	0xF160,
-	0xF140,
-	0xF100,
-	0xF0E0,
-	0xF0C0,
-	0xF0A0,
-	0xF080,
-	0xF060,
-	0xF040,
-	0xF020,
-	0xF800,
-};
+#include <colors.h>
 
 // WebServer server(80);
 //  Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
 // Enter your SSID and PASSWORD
-const char *ssid = "ESP";
-const char *password = "camera";
+// const char *ssid = "ESP";
+// const char *password = "camera";
 
 float MaxTemp = 0;
 float MinTemp = 0;
@@ -329,104 +77,6 @@ String getMinTemp() {
 	return String(MinTemp);
 }
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-    img { 
-      display: inline-block;
-      vertical-align: middle;
-      max-height: 100%;
-      max-width: 100%;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP32 Thermal Camera</h2>
-  <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature: </span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup><br>
-    
-    <span class="dht-labels">Temp Max: </span> 
-    <span id="tempmax">%TEMPMAX%</span>
-    <sup class="units">&deg;C</sup><br>
-    
-    <span class="dht-labels">Temp Min: </span> 
-    <span id="tempmin">%TEMPMIN%</span>
-    <sup class="units">&deg;C</sup>
-
-    <img src="thermal" id="thermalimage" style="width: 100%">
-  </p>
-</body>
-<script>
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 1000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("tempmax").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/tempmax", true);
-  xhttp.send();
-}, 1000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("tempmin").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/tempmin", true);
-  xhttp.send();
-}, 1000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    //if (this.readyState == 4 && this.status == 200) {
-      var image = document.getElementById("thermalimage");
-      //image.src = this.response;
-      image.src = "thermal";
-      //document.getElementById("thermal") = this.response;
-    //}
-  };
-  xhttp.open("GET", "/thermal", true);
-  xhttp.send();
-}, 1000 ) ;
-
-</script>
-</html>)rawliteral";
 
 // Replaces placeholder with values
 String processor(const String &var) {
@@ -451,8 +101,6 @@ boolean isConnected() {
 		return (false);  // Sensor did not ACK
 	return (true);
 }
-
-// void getThermalImage(){ }
 
 void ThermalImageToWeb(float mlx90640To[], float MinTemp, float MaxTemp) {
 	// --- SAVE BMP FILE --- //
@@ -595,55 +243,51 @@ void MLX_to_Serial(float mlx90640To[]) {
 //==========================================================================
 
 void setup() {
-	Wire.begin();
-	Wire.setClock(400000);  // Increase I2C clock speed to 400kHz
-	Serial.begin(115200);
-	while (!Serial)
-		;  // Wait for user to open terminal
+	DEBUGINIT();
+	mySmartConfig();
 
-	if (!SPIFFS.begin(true)) {
-		Serial.println("An Error has occurred while mounting SPIFFS");
-		return;
-	}
+	Wire.begin(I2C_SDA, I2C_SCL,400000);
+	// Wire.begin();
+	//	Wire.setClock(400000);  // Increase I2C clock speed to 400kHz
+
 	// ESP32 As access point
-	WiFi.mode(WIFI_AP);  // Access Point mode
-	WiFi.softAP(ssid, password);
+	// WiFi.mode(WIFI_AP);  // Access Point mode
+	// WiFi.softAP(ssid, password);
 
-	Serial.print("SDA pin: ");
-	Serial.println(SDA);
-	Serial.print("SCL pin: ");
-	Serial.println(SCL);
+	if (isConnected() )
+		DEBUGLOG("MLX90640 online !\n");
+	else
+		DEBUGLOG("MLX90640 not detected at default I2C address. Please check wiring.\n");
 
-	Serial.println("MLX90640 IR Array Example");
-
-	if (isConnected() == false) {
-		Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
-		while (1)
-			;
-	}
-	Serial.println("MLX90640 online!");
+	if (SPIFFS.begin(true))
+		DEBUGLOG("SPIFFS mounted !\n");
+	else
+		DEBUGLOG("An Error has occurred while mounting SPIFFS\n");
 
 	// Get device parameters - We only have to do this once
 	int status = 0;
 	uint16_t eeMLX90640[832];
 	status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
-	if (status != 0)
-		Serial.println("Failed to load system parameters");
+	if (status)
+		DEBUGLOG("MLX90640 Failed to load system parameters\n");
+	else
+		DEBUGLOG("MLX90640 system parameters loaded\n");
 
 	status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
-	if (status != 0)
-		Serial.println("Parameter extraction failed");
+	if (status)
+		DEBUGLOG("MLX90640 Parameter extraction failed\n");
+	else
+		DEBUGLOG("MLX90640 Parameter extracted\n");
 
-	int SetRefreshRate = 0;
-	SetRefreshRate = MLX90640_SetRefreshRate(0x33, 0x03);
+	int SetRefreshRate = MLX90640_SetRefreshRate(MLX90640_address, 0x03);
 	// int SetInterleavedMode = MLX90640_SetInterleavedMode(MLX90640_address);
 	int SetChessMode = MLX90640_SetChessMode(MLX90640_address);
 
 	// --- Part WebServer ESP --- //
 
-	IPAddress ServerIP = WiFi.softAPIP();  // Obtain the IP of the Serve
-	Serial.print("IP address: ");
-	Serial.println(ServerIP);  // IP address assigned to your ESP
+	// IPAddress ServerIP = WiFi.softAPIP();  // Obtain the IP of the Serve
+	// Serial.print("IP address: ");
+	// Serial.println(ServerIP);  // IP address assigned to your ESP
 	//----------------------------------------------------------------
 
 	// Route for root / web page
@@ -668,6 +312,7 @@ void setup() {
 	Serial.println("HTTP server started");
 
 	ArduinoOTA.begin();
+	DEBUGLOG("Setup Done\n");
 }
 
 // LOOP
@@ -697,7 +342,7 @@ void loop() {
 
 	// --- START of Calculate Chess Mode --- //
 	// Calculate difference between Subpages (chess-mode)
-	/*    int pa = 0;
+	    int pa = 0;
 		int niepa = 0;
 		float sumpa = 0;
 		float sumniepa = 0;
@@ -743,7 +388,7 @@ void loop() {
 			  }
 			}
 		  }
-		}       */
+		}       
 	// --- END of Calculate Chess Mode --- //
 
 	CenterTemp = (mlx90640To[165] + mlx90640To[180] + mlx90640To[176] + mlx90640To[192]) / 4.0;  // Temp in Center - based on 4 pixels
